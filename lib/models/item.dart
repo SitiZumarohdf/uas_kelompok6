8d8f7c72dca699f78274a7b38be2fb832b4487e2
= <=>
2022-12-28 18:37:45 +0700

    update item dart var type

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Item {
-  int? nim;
-  String? nama;
-  String? alamat;
-  String? jenisKelamin;
+  late int _nim;
+  late String _nama;
+  late String _alamat;
+  late String _jenisKelamin;
 
-  // String get nim => this._nim;
-  // set nim(String value) => this._nim = value;
+  int get nim => this._nim;
+  set nim(int value) => this._nim = value;
 
-  // String get nama => this._nama;
-  // set nama(String value) => this._nama = value;
+  String get nama => this._nama;
+  set nama(String value) => this._nama = value;
 
-  // String get alamat => this._alamat;
-  // set alamat(String value) => this._alamat = value;
+  String get alamat => this._alamat;
+  set alamat(String value) => this._alamat = value;
 
-  // String get jenisKelamin => this._jenisKelamin;
-  // set jenisKelamin(String value) => this._jenisKelamin = value;
+  String get jenisKelamin => this._jenisKelamin;
+  set jenisKelamin(String value) => this._jenisKelamin = value;
 
   // Konstruksi versi 1
-  Item(this.nim, this.nama, this.alamat, this.jenisKelamin);
+  Item(this._nim, this._nama, this._alamat, this._jenisKelamin);
 
   // Konstruktor versi 2 : konversi dari map ke item
   Item.fromMap(Map<String, dynamic> map) {
-    this.nim = map['nim'];
-    this.nama = map['nama'];
-    this.alamat = map['alamat'];
-    this.jenisKelamin = map['jenisKelamin'];
+    this._nim = map['nim'];
+    this._nama = map['nama'];
+    this._alamat = map['alamat'];
+    this._jenisKelamin = map['jenisKelamin'];
   }
 
   // Konversi dari item ke map
